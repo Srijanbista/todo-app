ca5dfb81fda140224954e4d28f781bd52576eaa6
Srijan Bista <Srijanbista4284@gmail.com>
2022-07-04 21:20:06 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import Todo from './Todo.jsx'
+
+function App() {
+
+
+  return (
+    <>
+      <Todo />
+    </>
+  )
+}
+
+export default App
